ace858b6fd74acc2e39f9a9e732fcbdb93a66bd6
quadrat1c <w4rd3nbot@gmail.com>
2019-02-09 10:44:53 -0800

    Update QuadHeals.js

--- QuadHeals.js
+++ QuadHeals.js
@@ -1,65 +1,42 @@
-// Hey there!
-// This is CODE, lets you control your character with code.
-// If you don't know how to code, don't worry, It's easy.
-// Just set attack_mode to true and ENGAGE!
+var auto_mode = true;
 
-var attack_mode=false
-var follow_mode=true
+/*
+ * TODO:
+ * Auto Party
+ * Auto Buy potions
+ * Auto Upgrade Items
+ */
 
-setInterval(function(){
-	var leader = get_player("Quadrat1c");
-	var mage = get_player("QuadMage");
-	
-	if (follow_mode) {
-		move(
-			character.x+(leader.x-character.x)/2,
-			character.y+(leader.y-character.y)/2
-			);
-	}
-	
-	if (leader.hp<500) heal(leader);
-	if (mage.hp<200) heal(mage);
-	if (character.hp<200) heal(character);
+setInterval(function () {
+    // create character entities
+    var leader = get_player("Quadrat1c");
+    var mage = get_player("QuadMage");
 
-	//use_hp_or_mp();
-	if(character.hp<100 || character.mp<80) use_hp_or_mp();
-	loot();
+    // heal self and other characters
+    if (character.hp<250) heal(character);
+    if (leader.hp<500) heal(leader);
+    if (mage.hp<200) heal(mage);
 
-	if(!attack_mode || character.rip || is_moving(character)) return;
+    // use potions
+    if(character.hp<100 || character.mp<80) use_hp_or_mp();
 
-	var target=get_targeted_monster();
-	if(!target)
-	{
-		target=get_nearest_monster({min_xp:100,max_att:120});
-		if(target) change_target(target);
-		else
-		{
-			set_message("No Monsters");
-			return;
-		}
-	}
-	
-	if(!in_attack_range(target))
-	{
-		move(
-			character.x+(target.x-character.x)/2,
-			character.y+(target.y-character.y)/2
-			);
-		// Walk half the distance
-	}
-	else if(can_attack(target))
-	{
-		set_message("Attacking");
-		attack(target);
-	}
+    loot();
 
-},1000/4); // Loops every 1/4 seconds.
+    if(!auto_mode || character.rip || is_moving(character)) return;
 
-// Learn Javascript: https://www.codecademy.com/learn/learn-javascript
-// Write your own CODE: https://github.com/kaansoral/adventureland
-// NOTE: If the tab isn't focused, browsers slow down the game
-// NOTE: Use the performance_trick() function as a workaround
+    var target = get_target_of(leader);
+    if (!target) {
+        // do nothing
+    } else {
+        if (can_attack(target)) {
+            set_message("Attacking");
+            attack(target);
+        }
+    }
 
-// Write your own CODE: https://github.com/kaansoral/adventureland
-// NOTE: If the tab isn't focused, browsers slow down the game
-// NOTE: Use the performance_trick() function as a workarou
+    move(
+        character.x+(leader.x-character.x)/2,
+        character.y+(leader.y-character.y)/2
+    );
+
+},1000/4);  // Loops every 1/4 seconds.
