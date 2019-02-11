26d2052e3a347a2a5f0f9da1091401074f8627fe
quadrat1c <w4rd3nbot@gmail.com>
2019-02-11 09:46:31 -0800

    Update QuadMage.js

--- QuadMage.js
+++ QuadMage.js
@@ -1,4 +1,7 @@
 var auto_mode = true;
+var assist_attack = true;
+
+//send_item("QuadMage", 4, 1);
 
 /*
  * TODO:
@@ -8,17 +11,30 @@ var auto_mode = true;
  */
 
 setInterval(function () {
+	if (!!Object.keys(parent.party).length == true) {
+		// do nothing
+	} else {
+		accept_party_invite("Quadrat1c");
+		game_log("Waiting for invite to party.");
+	}
+	
     // create character entities
     var leader = get_player("Quadrat1c");
 
     // use potions
-    if(character.hp<100 || character.mp<80) use_hp_or_mp();
+    if(character.hp < 100 || character.mp < 300) use_hp_or_mp();
 
     loot();
 
     if(!auto_mode || character.rip || is_moving(character)) return;
 
-    var target = get_target_of(leader);
+    var target;
+	if (assist_attack) { 
+		target = get_target_of(leader); 
+	} else {
+		target = get_nearest_monster({min_xp:100,max_att:100});
+	}
+	
     if (!target) {
         // do nothing
     } else {
@@ -43,3 +59,11 @@ setInterval(function () {
 	}
 
 },1000/4);  // Loops every 1/4 seconds.
+
+function assist() {
+	if (assist_attack) {
+		assist_attack = false;
+	} else {
+		assist_attack = true;
+	}
+}
